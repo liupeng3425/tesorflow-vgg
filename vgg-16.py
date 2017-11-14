4e0156d079baf80e74989066e14ad1253b0bbbcf
peng <liupeng3425@qq.com>
2017-11-14 19:14:24 +0800

    debug

--- vgg-16.py
+++ vgg-16.py
@@ -36,31 +36,31 @@ conv1 = tf.nn.relu(conv(data, gen_variable('w_conv1', [3, 3, 3, 64])) + gen_vari
 conv2 = tf.nn.relu(conv(conv1, gen_variable('w_conv2', [3, 3, 64, 64])) + gen_variable('b_conv2', [64]))
 
 max_pool_2 = max_pool(conv2, 'max_pool_2')
+#
+# conv3 = tf.nn.relu(conv(max_pool_2, gen_variable('w_conv3', [3, 3, 64, 128])) + gen_variable('b_conv3', [128]))
+# conv4 = tf.nn.relu(conv(conv3, gen_variable('w_conv4', [3, 3, 128, 128])) + gen_variable('b_conv4', [128]))
+#
+# # max_pool_4 = max_pool(conv4, 'max_pool_4')
+#
+# conv5 = tf.nn.relu(conv(conv4, gen_variable('w_conv5', [3, 3, 128, 256])) + gen_variable('b_conv5', [256]))
+# conv6 = tf.nn.relu(conv(conv5, gen_variable('w_conv6', [3, 3, 256, 256])) + gen_variable('b_conv6', [256]))
+# conv7 = tf.nn.relu(conv(conv6, gen_variable('w_conv7', [3, 3, 256, 256])) + gen_variable('b_conv7', [256]))
+#
+# max_pool_7 = max_pool(conv7, 'max_pool_7')
+#
+# conv8 = tf.nn.relu(conv(max_pool_7, gen_variable('w_conv8', [3, 3, 256, 512])) + gen_variable('b_conv8', [512]))
+# conv9 = tf.nn.relu(conv(conv8, gen_variable('w_conv9', [3, 3, 512, 512])) + gen_variable('b_conv9', [512]))
+# conv10 = tf.nn.relu(conv(conv9, gen_variable('w_conv10', [3, 3, 512, 512])) + gen_variable('b_conv10', [512]))
+#
+# # max_pool_9 = max_pool(conv10, 'max_pool_9')
+#
+# conv11 = tf.nn.relu(conv(conv10, gen_variable('w_conv11', [3, 3, 512, 512])) + gen_variable('b_conv11', [512]))
+# conv12 = tf.nn.relu(conv(conv11, gen_variable('w_conv12', [3, 3, 512, 512])) + gen_variable('b_conv12', [512]))
+# conv13 = tf.nn.relu(conv(conv12, gen_variable('w_conv13', [3, 3, 512, 512])) + gen_variable('b_conv13', [512]))
+#
+# max_pool_9 = max_pool(conv13, 'max_pool_9')
 
-conv3 = tf.nn.relu(conv(max_pool_2, gen_variable('w_conv3', [3, 3, 64, 128])) + gen_variable('b_conv3', [128]))
-conv4 = tf.nn.relu(conv(conv3, gen_variable('w_conv4', [3, 3, 128, 128])) + gen_variable('b_conv4', [128]))
-
-# max_pool_4 = max_pool(conv4, 'max_pool_4')
-
-conv5 = tf.nn.relu(conv(conv4, gen_variable('w_conv5', [3, 3, 128, 256])) + gen_variable('b_conv5', [256]))
-conv6 = tf.nn.relu(conv(conv5, gen_variable('w_conv6', [3, 3, 256, 256])) + gen_variable('b_conv6', [256]))
-conv7 = tf.nn.relu(conv(conv6, gen_variable('w_conv7', [3, 3, 256, 256])) + gen_variable('b_conv7', [256]))
-
-max_pool_7 = max_pool(conv7, 'max_pool_7')
-
-conv8 = tf.nn.relu(conv(max_pool_7, gen_variable('w_conv8', [3, 3, 256, 512])) + gen_variable('b_conv8', [512]))
-conv9 = tf.nn.relu(conv(conv8, gen_variable('w_conv9', [3, 3, 512, 512])) + gen_variable('b_conv9', [512]))
-conv10 = tf.nn.relu(conv(conv9, gen_variable('w_conv10', [3, 3, 512, 512])) + gen_variable('b_conv10', [512]))
-
-# max_pool_9 = max_pool(conv10, 'max_pool_9')
-
-conv11 = tf.nn.relu(conv(conv10, gen_variable('w_conv11', [3, 3, 512, 512])) + gen_variable('b_conv11', [512]))
-conv12 = tf.nn.relu(conv(conv11, gen_variable('w_conv12', [3, 3, 512, 512])) + gen_variable('b_conv12', [512]))
-conv13 = tf.nn.relu(conv(conv12, gen_variable('w_conv13', [3, 3, 512, 512])) + gen_variable('b_conv13', [512]))
-
-max_pool_9 = max_pool(conv13, 'max_pool_9')
-
-flat = tf.reshape(max_pool_9, [-1, 8192])
+flat = tf.reshape(max_pool_2, [64, -1])
 dim = flat.get_shape()[1].value
 fc14 = tf.nn.relu(tf.matmul(flat, gen_variable('w_fc14', [dim, 4096])) +
                   gen_variable('b_fc14', [4096]))
