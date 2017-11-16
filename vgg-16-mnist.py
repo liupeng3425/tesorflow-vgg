282d4d5e66540cbc856a5c81936590c3f922adfc
peng <liupeng3425@qq.com>
2017-11-16 20:02:51 +0800

    decrease stddev

--- vgg-16-mnist.py
+++ vgg-16-mnist.py
@@ -2,11 +2,10 @@ import os
 
 import numpy
 import tensorflow as tf
-
-from kits import utils
+import tensorflow.examples.tutorials.mnist.input_data as input_data
 
 PATH = os.path.dirname(__file__)
-PATH = os.path.join(PATH, 'cifar-10-batches-py')
+PATH = os.path.join(PATH, 'MNIST_data')
 BATCH_SIZE = 64
 
 
@@ -28,31 +27,32 @@ def bias_variable(name, shape):
     return tf.Variable(initial, name=name)
 
 
-block_filter_num = (64, 128, 128, 256, 256)
+block_filter_num = (64, 128, 128, 32, 32)
 fc_neural_num = (128, 128, 64)
 softmax_output_num = 10
 
-num_of_maxpool = 5
+num_of_maxpool = 1
 
-data = tf.placeholder(tf.float32, [None, 32, 32, 3], 'input')
+data = tf.placeholder(tf.float32, [None, 28 * 28 * 1], 'input')
+reshape = tf.reshape(data, (-1, 28, 28, 1))
 
 conv1 = tf.nn.relu(
-    conv(data, weight_variable('w_conv1', [3, 3, 3, block_filter_num[0]])) +
+    conv(reshape, weight_variable('w_conv1', [3, 3, 1, block_filter_num[0]])) +
     bias_variable('b_conv1', [block_filter_num[0]]))
 conv2 = tf.nn.relu(
     conv(conv1, weight_variable('w_conv2', [3, 3, block_filter_num[0], block_filter_num[0]])) +
     bias_variable('b_conv2', [block_filter_num[0]]))
 
-max_pool_2 = max_pool(conv2, 'max_pool_2')
+# max_pool_2 = max_pool(conv2, 'max_pool_2')
 
-conv3 = tf.nn.relu(conv(max_pool_2, weight_variable('w_conv3', [3, 3, block_filter_num[0], block_filter_num[1]])) +
+conv3 = tf.nn.relu(conv(conv2, weight_variable('w_conv3', [3, 3, block_filter_num[0], block_filter_num[1]])) +
                    bias_variable('b_conv3', [block_filter_num[1]]))
 conv4 = tf.nn.relu(conv(conv3, weight_variable('w_conv4', [3, 3, block_filter_num[1], block_filter_num[1]])) +
                    bias_variable('b_conv4', [block_filter_num[1]]))
 
-max_pool_4 = max_pool(conv4, 'max_pool_4')
+# max_pool_4 = max_pool(conv4, 'max_pool_4')
 
-conv5 = tf.nn.relu(conv(max_pool_4, weight_variable('w_conv5', [3, 3, block_filter_num[1], block_filter_num[2]])) +
+conv5 = tf.nn.relu(conv(conv4, weight_variable('w_conv5', [3, 3, block_filter_num[1], block_filter_num[2]])) +
                    bias_variable('b_conv5', [block_filter_num[2]]))
 conv6 = tf.nn.relu(conv(conv5, weight_variable('w_conv6', [3, 3, block_filter_num[2], block_filter_num[2]])) +
                    bias_variable('b_conv6', [block_filter_num[2]]))
@@ -68,20 +68,20 @@ conv9 = tf.nn.relu(conv(conv8, weight_variable('w_conv9', [3, 3, block_filter_nu
 conv10 = tf.nn.relu(conv(conv9, weight_variable('w_conv10', [3, 3, block_filter_num[3], block_filter_num[3]])) +
                     bias_variable('b_conv10', [block_filter_num[3]]))
 
-max_pool_9 = max_pool(conv10, 'max_pool_9')
+# max_pool_9 = max_pool(conv10, 'max_pool_9')
 
-conv11 = tf.nn.relu(conv(max_pool_9, weight_variable('w_conv11', [3, 3, block_filter_num[3], block_filter_num[4]])) +
+conv11 = tf.nn.relu(conv(conv10, weight_variable('w_conv11', [3, 3, block_filter_num[3], block_filter_num[4]])) +
                     bias_variable('b_conv11', [block_filter_num[4]]))
 conv12 = tf.nn.relu(conv(conv11, weight_variable('w_conv12', [3, 3, block_filter_num[4], block_filter_num[4]])) +
                     bias_variable('b_conv12', [block_filter_num[4]]))
 conv13 = tf.nn.relu(conv(conv12, weight_variable('w_conv13', [3, 3, block_filter_num[4], block_filter_num[4]])) +
                     bias_variable('b_conv13', [block_filter_num[4]]))
 
-max_pool_13 = max_pool(conv13, 'max_pool_13')
+# max_pool_13 = max_pool(conv13, 'max_pool_13')
 
-dim = block_filter_num[4] * data.get_shape()[1].value * data.get_shape()[1].value / (2 ** (2 * num_of_maxpool))
+dim = block_filter_num[4] * reshape.get_shape()[1].value * reshape.get_shape()[1].value / (2 ** (2 * num_of_maxpool))
 dim = int(dim)
-flat = tf.reshape(max_pool_13, [-1, dim])
+flat = tf.reshape(conv13, [-1, dim])
 fc14 = tf.nn.relu(tf.matmul(flat, weight_variable('w_fc14', [dim, fc_neural_num[0]])) +
                   bias_variable('b_fc14', [fc_neural_num[0]]))
 
@@ -102,20 +102,20 @@ train_step = tf.train.AdamOptimizer(1e-2).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(softmax, 1), tf.argmax(y_label, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
-data_set = utils.read_data(PATH)
+data_set = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 for i in range(40000):
-    batch = data_set.next_batch_data(BATCH_SIZE)
+    batch = data_set.train.next_batch(BATCH_SIZE)
 
     # for var in tf.trainable_variables():
     #     print(var)
 
     if i % 250 == 0:
         loss, train_accuracy, prediction = sess.run([cross_entropy, accuracy, softmax],
-                                                    feed_dict={data: batch['data'],
-                                                               y_label: batch['labels_one_hot']})
+                                                    feed_dict={data: batch[0],
+                                                               y_label: batch[1]})
         print("step %d, training accuracy %g, cross entropy %g" % (i, train_accuracy, loss))
         # print(tf.get_default_graph().get_tensor_by_name('w_conv1:0').eval()[0][0][0][0])
         print('prediction:')
@@ -124,9 +124,9 @@ for i in range(40000):
         print(numpy.sum(prediction[0:2], axis=1))
     if i % 500 == 0:
         print("test accuracy %g" % accuracy.eval(feed_dict={
-            data: data_set.test_set['data'][0:2000], y_label: data_set.test_set['labels_one_hot'][0:2000]}))
+            data: data_set.test.images[1000:2000], y_label: data_set.test.labels[1000:2000]}))
 
-    train_step.run(feed_dict={data: batch['data'], y_label: batch['labels_one_hot']})
+    train_step.run(feed_dict={data: batch[0], y_label: batch[1]})
 
 print("test accuracy %g" % accuracy.eval(feed_dict={
-    data: data_set.test_set['data'][4000:8000], y_label: data_set.test_set['labels_one_hot'][4000:8000]}))
+    data: data_set.test.images[1000:3000], y_label: data_set.test.labels[1000:3000]}))
